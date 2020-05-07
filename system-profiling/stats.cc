be577167b5531ad608d27331c5e3700b65bed7a0
Steven Chen <steven@alphash.io>
2020-05-07 23:34:05 +0800

    feat: retrieve file system info in a programmatic way

--- system-profiling/stats.cc
+++ system-profiling/stats.cc
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <filesystem>    // '-std=c++17'
 
 #include <assert.h>
 #include <string.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/statvfs.h>
 #include <sys/sysctl.h>
+
+namespace fs = std::filesystem;
 
 // https://opensource.apple.com/source/xnu/xnu-201.5/bsd/sys/sysctl.h.auto.html
 // https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man3/sysctl.3.html
@@ -32,11 +36,34 @@ int main() {
   get_counter("hw.byteorder", 2, byteorder);
   get_counter("hw.pagesize", 2, pagesize);
 
+  printf("-------- kernel and hardware info --------\n");
   printf("Max %ld concurrent process supported\n", maxproc);
   printf("Total %ld CPUs installed\n", ncpu);
   printf("Total %ld bytes of physical memory\n", physmem);
   printf("Total %ld bytes of non-kernel memory\n", usermem);
   printf("The byteorder (little endian: 4321 or big endian: 1234) %ld\n", byteorder);
   printf("The software page size is %ld bytes\n", pagesize);
+
+  // Get file system information
+  const char *path = "/"; // any path under the mount point would work
+  struct statvfs buf;
+  statvfs(path, &buf);
+  printf("-------- file system info --------\n");
+  printf("file system block size: %ld\n", buf.f_bsize);
+  printf("fragment size: %ld\n", buf.f_frsize);
+  printf("Size of fs in f_frsize units: %d\n", buf.f_blocks);
+  printf("file system ID: %ld\n", buf.f_fsid);
+  printf("Number of free blocks: %d\n", buf.f_bfree);
+  printf("Number of free blocks for unprivileged users: %d\n", buf.f_bavail);
+  printf("Number of inodes (file): %d\n", buf.f_files);
+  printf("Number of free inodes: %d\n", buf.f_ffree);
+  printf("Number of free inodes for unprivileged users: %d\n", buf.f_favail);
+  printf("mount flags: %ld\n", buf.f_flag);
+  printf("Maximum filename length: %ld\n", buf.f_namemax);
+
+  // Get the space info of specific path
+  fs::space_info tmp = fs::space("/tmp");
+  std::cout << "Free space: " << tmp.free << '\n'
+            << "Available space: " << tmp.available << '\n';
   return 0;
 }
